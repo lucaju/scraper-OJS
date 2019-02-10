38f299a70cda335ff11c5cd7a580b97d0eca94b8
Luciano Frizzera <lucaju@me.com>
2019-02-10 01:45:47 -0500

    save

--- index.js
+++ index.js
@@ -1,267 +1,422 @@
 /*jshint esversion: 6 */
 
 //Modules
+const rp = require('request-promise');
 const cheerio = require('cheerio');
-require('colors');
+const jsonfile = require('jsonfile');
 const csvdata = require('csvdata');
+const readline = require('readline');
+const colors = require('colors');
 const fs = require('fs');
  
+
 //-------------
 
-//Initialize variables\
+//Initialize variables
+let dataset = [];
 
 let datetime = new Date();
-datetime = `${datetime.getFullYear()}-${(datetime.getMonth()+1)}-${datetime.getDate()}`;
+datetime = `${datetime.getDate()}-${(datetime.getMonth()+1)}-${datetime.getFullYear()}`;
 
+let externalPageNumber = 0;
 
-const targets = [
-	{
-		name: 'Datasets',
-		url: 'https://www.kaggle.com/datasets?sortBy=votes&group=public&page=1&pageSize=20&size=all&filetype=all&license=all'
-	},
-	{
-		name: 'Competitions',
-		url: 'https://www.kaggle.com/competitions?sortBy=numberOfTeams&group=general&page=1&pageSize=20'
-	},
-	{
-		name: 'User: Competition',
-		url: 'https://www.kaggle.com/rankings?group=competitions&page=1&pageSize=20'
-	}
-];
+let targetWebsite = {
+  name: "Compos",
+  baseUrl:"http://www.e-compos.org.br/e-compos/"
+};
+let pageLimit = 10; // default:10 // -1 -> no limit
+let loadDetails = true; // default:10 // -1 -> no limit
 
 let rpOptions = {
-	transform: function (body) {
-		return cheerio.load(body);
-	}
+  transform: function (body) {
+    return cheerio.load(body);
+  }
 };
+
+
+//----------------------------
+// passing variables 
+
+//asesse Variables passed throug command line (e.g, node index limit=10)
+// process.argv.forEach((val, index) => {
+//  //limit
+//  if (val.substr(0,6) == "limit=") {
+//    setLimit(val);
+//  }
+// });
+
+// //set page limit
+// function setLimit(val) {
+//  let limit = val.split("=");
+//  let l = +limit[1];
+//  if(Number.isInteger(l)) {
+//    pageLimit = l;
+//    console.log(`Page limit set to ${l}.`);
+//  } else {
+//    console.log("Page limit must be a number. Default: 10 // Unlimited: -1 .");
+//  }
+// }
 
 //----------------------------
 //Initical Setup
+console.log("Webscraper for Open Journal System".green);
+
+setup();
+
+//setup question in the beginning
+function setup() {
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
 
-initScrapper();
+  rl.question(`Target website name? ` + `(Default: ${targetWebsite.name}): `.grey, (name) => {
+    if (name.length > 0) targetWebsite.name = name;
+  
+    rl.question(`Target website url? ` + `(Default: ${targetWebsite.baseUrl}): `.grey, (url) => {
+      if (url.length > 0) targetWebsite.baseUrl = url;
+    
+      rl.question(`Page limit? ` + ` (Default: ${pageLimit}. Unlimited: -1): `.grey, (limit) => {
+        if (limit.length > 0) pageLimit = limit;
+        
+        initScrapper();
+        rl.close();
+      });
+    });
+    
+  });
+}
 
 //----------------------------//
 //initiate
 
-//delay load pages to scrapper
-function sleeper(ms) {
-	return function (x) {
-		return new Promise(resolve => setTimeout(() => resolve(x), ms));
-	};
-}
-
 // initScrapper();
 function initScrapper() {
-	console.log('Scraper for Kaggle'.green);
-	console.log('-----------------');
-	console.log('Initializing scrapping.');
+  console.log(`-----------------`);
+  console.log(`Initializing scrapping.`);
+  console.log(`Journal: ${targetWebsite.name} | Website: ${targetWebsite.baseUrl}`);
+  console.log(`-----------------`);
+  console.log(`Scraping archive pages.`);
+    addPageToScrape();
+}
 
-	for (const target of targets) {
-		console.log(`Top ${target.name} | URL: ${target.url}`);
-		loadURL(target);
-	}
+//add external page
+function addPageToScrape() {
+  externalPageNumber++;
+  rpOptions.uri = `${targetWebsite.baseUrl}search/titles?searchPage=${externalPageNumber}#results`;
+  loadURL("external");
+}
+
+//delay load pages to scrapper
+function sleeper(ms) {
+  return function(x) {
+    return new Promise(resolve => setTimeout(() => resolve(x), ms));
+  };
+  }
+
+//load 
+function loadURL(type, articleID) {
+
+  //loadd promise
+  rp(rpOptions)
+    .then(sleeper(100))
+    .then(($) => {
+      
+      //extennal
+      if(type == "external") {
+
+        //Test if page has results
+        //Parse results, or move to load results details
+        let error404 = $("#results").find(".nodata");
+        
+        if(error404.length == 0) {
+          parseExternal($);
+        } else {
+          console.log(`Page limit rechead.` + ` (increase pagelimit for more)`.grey);
+          console.log(`--------------`);
+
+          //if scraping details ..
+          if (loadDetails == true) {
+            console.log(`Scraping details.`);
+            loadNextDetails();
+          } else {
+            finish();
+          }
+
+        }
+
+      } else {
+
+        //internal
+        parseInternal($,articleID);
+      }
+
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
 }
 
-function loadURL(target) {
-	rpOptions.uri = target.url;
+//parse external page
+function parseExternal($) {
 
-	//loadd promise
-	rp(rpOptions)
-		.then(sleeper(100))
-		.then(($) => {
+  const pageN = externalPageNumber;
+  console.log(`scraping page: ${pageN}`);
 
-			if (target.name == 'Datasets') {
-				scrapeTopDatasets($);
-			} else if (target.name == 'Competitions') {
-				scrapeTopChallenges($);
-			} else if (target.name == 'User: Competition') {
-				scrapeTopUser($);
-			}
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+  const lines = $('#results').find("tr"); //lines
+
+  lines.each(function(i, line) {
+
+    line = $(line);
+
+    //only gets the lines with information.
+    //edition, number, title, and link to abstract are in the lines with the attribute 'valign' = 'top'
+    //author name is in the subsequent line
+
+    if (line.attr("valign") == "top") {
+      
+      //cells in each line
+      const cells = $(line.find('td'));
+
+      //variables to save
+      let articleID, volume, number, year, title, url, pdf, authors;
+      
+      // 1. volume and number is in the first column
+      let cellEdition = $(cells[0]);
+      cellEdition = $(cellEdition.find("a"));
+      let edition = cellEdition.text();
+
+      if (edition == "Ahead of Print") {
+        volume = edition;
+      } else {
+        let yearRegex = /\(\b(\d{4})\)/; //(2014)
+        let yearRegexMatch = edition.match(yearRegex);
+        year = yearRegexMatch[1];
+
+        let volRegex = /v. (\d{1,2})/; //v. 2
+        let volumeMatch = edition.match(volRegex);
+        if (volumeMatch) volume = volumeMatch[1];
+
+        let numRegex = /n. (\d{1,2})/; // n. 3
+        let numberMatch = edition.match(numRegex);
+        if (numberMatch != null) number = numberMatch[1];
+
+      }
+
+      //2. title is in the second cell// 
+      title = $(cells[1]).text();
+      
+       //3.  url is in the thirs cell//
+      let urls = $(cells[2]).find("a");
+      url = $(urls[0]).attr("href");
+      pdf = $(urls[1]).attr("href");
+
+      //4. get item ID from URL
+      let idRegex = /\/(\d{1,})/; 
+      let idMatch = url.match(idRegex);
+      articleID = idMatch[1];
+
+      //get author in the next line
+      let authorsLine = $(line.next());
+      let authorCell = $(authorsLine[0]).find("td");
+      authors = $(authorCell).text();
+      
+       authors = authors.trim(); // remove white space
+       authors = authors.split(",");
+       for(let i=0; i<authors.length;i++) {
+         authors[i] = authors[i].trim();
+       }
+       
+       //data
+       let article = {
+        journal: targetWebsite.name,
+        articleID: articleID,
+        index: dataset.length,
+        authors: authors,
+        title: title,
+        volume: volume,
+        number: number,
+        year: year,
+        url: url,
+        pdf: pdf,
+        complete: false
+      };
+      
+      //sava data
+      dataset.push(article);
+      
+    }
+
+  });
+
+  //check page page limit
+  // if reach the limit, stop gathering external pages
+  if(pageLimit > 0 && externalPageNumber >= pageLimit) {
+    console.log(`Page limit rechead.` + ` (increase pagelimit for more)`.grey);
+    console.log(`--------------`);
+
+    //if scraping details ..
+    if (loadDetails == true) {
+      console.log(`Scraping details.`);
+      loadNextDetails();
+    } else {
+      finish();
+    }
+
+  } else {
+    addPageToScrape();
+  }
 
 }
 
-function scrapeTopDatasets($) {
-	
-	const script = $('.site-layout__main-content').find('script');
-	const data = script.html();
+//select next article to collect detail
+function loadNextDetails(articleIndex) {
 
-	//isolate data
-	const isolatedData = data.substring(77,data.length-93);
-	const jsonData = JSON.parse(isolatedData);
-	const list = jsonData.datasetListItems;
+  //if articleIndex is undefined, set to 0 (first call)
+  if (articleIndex == undefined) articleIndex = 0;
 
-	console.log('-----------------'.blue);
-	console.log('TOP DATASETS'.blue);
+  //contine to call the next if index is lower than the total
+  if (articleIndex < dataset.length) {
+    const article = dataset[articleIndex];
+    rpOptions.uri = `${targetWebsite.baseUrl}article/view/${article.articleID}`;
+    loadURL("internal",article.articleID);
+  } else {
+    finish();
+  }
+  
+}
 
-	const topDatasets = {
-		title: 'Kaggle-Top-Datasets',
-		dataset:[],
-		headers: 'title,downloads,views,votes,url'
-	};
-	
-	for (const item of list) {
-		topDatasets.dataset.push(extractDatasetInfo(item));
-	}
+//parse internal page
+function parseInternal($,articleID) {
 
-	getJson(topDatasets);
-	getCSV(topDatasets);
+  //call article on the list
+  let article = dataset.find(item => item.articleID == articleID);
+
+  console.log(`scraping article: ${articleID} (${article.index}/${dataset.length})`);
+
+  //variables
+  let title, authors, abstract, keywords, doi;
+
+  // 1. title
+    title = $("#articleTitle").text();
+  
+  // 2. Authors
+  authors = $("#authorString").text();
+  authors = authors.split(",");
+
+  // 3. abstract
+  abstract = $("#articleAbstract").find("div").text();
+  abstract = abstract.replace(/\n/g," "); ////check if there is line break
+
+   // 4. keywordsd
+  keywords = $("#articleSubject").find("div").text();
+  
+   //break into array .. three types of separation foundd in the website (,) or (;) or (.).. remove the last if empty 
+  keywords = keywords.split(",");
+  if (keywords.length == 1) keywords = keywords[0].split(";");
+  if (keywords.length == 1) keywords = keywords[0].split(".");
+  if (keywords[keywords.length-1] == "") keywords.pop();
+
+  // 5. DOI
+  doi = $("a[id='pub-id::doi']").text();
+  
+  //data
+  let details = {
+        title: title,
+        authors: authors,
+        abstract: abstract,
+        keywords: keywords,
+        doi: doi
+  };
+
+  //save  data
+  article.keywords = details.keywords;
+    article.abstract = details.abstract;
+    article.doi = details.doi;
+
+    article.details = details;
+  article.complete = true;
+  
+  //call next
+  const nextIndex = article.index + 1;
+  loadNextDetails(nextIndex);
 
 }
 
-function extractDatasetInfo(item) {
+//finish
+function finish() {
 
-	// console.log(item);
+  if (loadDetails == true) {
 
-	const Dataset = {
-		title: item.title,
-		downloads: item.downloadCount,
-		views: item.viewCount,
-		votes: item.voteButton.totalVotes,
-		url: 'https://www.kaggle.com' + item.datasetUrl
-	};
+    let articlesComplete = dataset.filter(article => article.complete == true);
 
-	return Dataset;
-}
+    console.log(`-----------------`);
+    console.log(`Done! ${articlesComplete.length}/${dataset.length}`);
+    console.log(`-----------------`);
+  }
 
+  //remove irrelevant data
+  for (const article of dataset) {
+    delete article.index;
+    delete article.complete;
+    delete article.details;
+  }
 
-function scrapeTopChallenges($) {
+  //parse, transform and save in both CSV and JSON
 
-	const script = $('.site-layout__main-content').find('script');
-	const data = script.html();
+  getCSV();
+  getJson();
 
-	// //isolate data
-	const isolatedData = data.substring(77,data.length-101);
-	const jsonData = JSON.parse(isolatedData);
-	const list = jsonData.pagedCompetitionGroup.competitions;
-
-	console.log('-----------------'.blue);
-	console.log('TOP COMPETITIONS'.blue);
-
-	const topCompetition = {
-		title: 'Kaggle-Top-Challenges',
-		dataset:[],
-		headers: 'title,organization,teams,url'
-	};
-	
-	for (const item of list) {
-		topCompetition.dataset.push(extractChallengeInfo(item));
-	}
-
-	getJson(topCompetition);
-	getCSV(topCompetition);
-
-}
-
-function extractChallengeInfo(item) {
-
-	// console.log(item);
-
-	const competition = {
-		title: item.competitionTitle,
-		organization: item.organizationName,
-		teams: item.totalTeams,
-		url: 'https://www.kaggle.com' + item.competitionUrl
-	};
-
-	return competition;
-}
-
-function scrapeTopUser($) {
-
-	const script = $('.site-layout__main-content').find('script');
-	const data = script.html();
-
-	// //isolate data
-	const isolatedData = data.substring(77,data.length-94);
-	const jsonData = JSON.parse(isolatedData);
-	const list = jsonData.list;
-
-	console.log('-----------------'.blue);
-	console.log('TOP USERS'.blue);
-
-	const topUsers = {
-		title: 'Kaggle-Top-Users-Competition',
-		dataset:[],
-		headers: 'name,ranking,tier,goldMedals,silverMedals,bronzeMedals,url'
-	};
-	
-	for (const item of list) {
-		topUsers.dataset.push(extractUserInfo(item));
-	}
-
-	getJson(topUsers);
-	getCSV(topUsers);
-
-}	
-
-function extractUserInfo(item) {
-
-	// console.log(item);
-
-	const user = {
-		name: item.displayName,
-		ranking: item.currentRanking,
-		tier: item.tier,
-		goldMedals: item.totalGoldMedals,
-		silverMedals: item.totalSilverMedals,
-		bronzeMedals: item.totalBronzeMedals,
-		url: 'https://www.kaggle.com' + item.userUrl
-	};
-
-	return user;
 }
 
 //get JSON
-function getJson(target) {
+function getJson() {
 
-	const fileName = `${target.title}-${datetime}.json`;
-	const folder = './results';
+  console.log(`Writing data to ${targetWebsite.name}-${datetime}.json`);
 
-	console.log(`Writing data to ${folder}/${fileName}`);
+  const folder = './results';
 
-	if (!fs.existsSync(folder)) fs.mkdirSync(folder);
+  if (!fs.existsSync(folder)) fs.mkdirSync(folder);
 
-	jsonfile.writeFile(`${folder}/${fileName}`, target.dataset, {
-		spaces: 4
-	}, function (err) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log('Json: Data written!'.green);
+  const file = `${targetWebsite.name}-${datetime}.json`;
 
-			console.log('-----------------');
-		}
-	});
+  jsonfile.writeFile(`${folder}/${file}`, dataset, {spaces: 4}, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Json: Data written!".green);
+
+      console.log(`-----------------`);
+    }
+  });
 
 }
 
 //get CSV
-function getCSV(target) {
+function getCSV() {
 
-	const fileName = `${target.title}-${datetime}.csv`;
-	const folder = './results';
+  const folder = './results';
 
-	if (!fs.existsSync(folder)) fs.mkdirSync(folder);
+  if (!fs.existsSync(folder)) fs.mkdirSync(folder);
 
-	//header
-	// const header = target.headers;
-	// csvdata.write(`${folder}/${file}`, header);
+  const file = `${targetWebsite.name}-${datetime}.csv`;
 
-	//body
-	let options = {
-		// append: false,
-		delimiter: ',',
-		header: target.headers,
-		log: true
-	};
+  //header
+  let header = 'journal|articleID|authors|title|volume|number|year|url|pdf|keywords|abstract|doi';
+  // csvdata.write(`${folder}/${file}`, header);
 
-	csvdata.write(`${folder}/${fileName}`, target.dataset, options);
+  //body
+  let options = {
+    // append: false,
+    delimiter: '|',
+    header: header,
+    log: true
+  };
 
-	console.log('-----------------');
+  csvdata.write(`${folder}/${file}`, dataset, options);
+
+  console.log(`-----------------`);
 }
